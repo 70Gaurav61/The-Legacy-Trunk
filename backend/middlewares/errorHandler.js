11552a8844383a41f8e401e1a2040a45579e5d28
Gaurav <gauravmahor7061@gmail.com>
2025-10-06 02:24:59 +0530

    Add controllers and routes for story

---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,7 @@
+export default (err, req, res, next) => {
+  console.error("Error:", err.message);
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Server Error",
+  });
+};
